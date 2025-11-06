cb04efbdd061be6ec30952997ea145f714ca4325
AliDemir140 <116540641+AliDemir140@users.noreply.github.com>
2025-11-06 22:21:09 +0300

    Add WEB Projects

---
+++ WEB/HTML/Program.cs
@@ -0,0 +1,10 @@
+
+WebApplicationBuilder builder=WebApplication.CreateBuilder(args);
+
+var app= builder.Build();
+
+//wwwroot klasörüne dýþarýdan (browser) ulaþýlmasýna  izin verilmesi gerekmektedir.
+
+app.UseStaticFiles(); //wwwroot dýþarýdan ulaþýlabilir hale gelir.
+
+app.Run();
